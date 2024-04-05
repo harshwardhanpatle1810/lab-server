5d0951e7713f8bec30b26ae08510842df6a3b0d3
harshwardhanpatle1810 <harshwardhanpatle1810@gmail.com>
2024-04-05 10:47:00 +0530

    lab server

--- index.js
+++ index.js
@@ -2,18 +2,46 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 const PORT =5000;
 
-app.get('/burger', (req, res) => {
-    res.send("burger is ready")
+app.get("/order/food",(req,res)=>{
+
+    const { menu,price,quantity} = req.query;
+    const {user, country} = req.headers;
+    const totalPrice = parseInt(price) + parseInt(quantity);
+
+    res.json({
+        message:`you have ordered ${quantity} ${menu}`,
+        bill:`Your Bill is ${totalPrice}`,
+        user:`your are ${user} from ${country}`
+    })
 })
 
-app.get('/pizza',(req,res) =>{
-    res.send("Pizza is ready")
+app.get("/food/:type",(req,res)=>{
+    
+    const{type}=req.params;
+   if (type==="veg"){
+    return res.json({
+        message:"you have order veg foog"
+    })
+   }
+
+    else{
+        return res.json({
+            message:"you have order non-veg food"
+        })
+    }
+
 })
 
-app.get('/pasta',(req,res )=>{
-    res.send("pasta is ready")
+app.post('/user',(req,res)=>{
+   const {name,age} = req.body;
+
+    res.json({
+        message:`hello ${name} ,you are ${age} Years`
+    })
 })
 
 app.listen(PORT, () => {
