57798c3f0d239c701224f94b4515989c570f9209
harshwardhanpatle1810 <harshwardhanpatle1810@gmail.com>
2024-04-05 17:36:59 +0530

    added

--- index.js
+++ index.js
@@ -45,6 +45,6 @@ app.post('/user',(req,res)=>{
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on the  port ${PORT}`);
 })
 
